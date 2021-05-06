038f76dcdd658f1d0da6230edc423c13d5a173cd
Bezsilny <julek200@gmail.com>
2021-05-06 17:41:05 +0200

    Added modal using React Portal

--- order_meal/src/components/Cart/Cart.js
+++ order_meal/src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal'
 import styles from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -5,7 +6,7 @@ const Cart = (props) => {
         { id: "c1", name: "Sushi", amount: 2, price: 12.99 },
     ].map((item) => <li>{item.name}</li>)}</ul>;
     return (
-        <div>
+        <Modal>
             {cartItems}
             <div className={styles.total}>
                 <span>Total amount</span>
@@ -15,7 +16,7 @@ const Cart = (props) => {
                 <button className={styles['button--alt']}>Close</button>
                 <button className={styles.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     );
 };
 export default Cart;
