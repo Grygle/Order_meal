5e5d673b3a40c134c8f5a04dcf9c0d93abd022ef
Bezsilny <julek200@gmail.com>
2021-05-07 17:11:15 +0200

    Using useContext() hook

--- order_meal/src/components/Layout/HeaderCartButton.js
+++ order_meal/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+        return currNumber + item.amount;
+    }, 0);
+
     return (
         <button className={styles.button} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={styles.badge}>7</span>
+            <span className={styles.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
