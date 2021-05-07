9062ce74aed9d0f2aa836b3868a4fac66a09ba32
Bezsilny <julek200@gmail.com>
2021-05-07 16:34:17 +0200

    Cart and Modal state management using useState() hook

--- order_meal/src/components/Cart/Cart.js
+++ order_meal/src/components/Cart/Cart.js
@@ -6,14 +6,14 @@ const Cart = (props) => {
         { id: "c1", name: "Sushi", amount: 2, price: 12.99 },
     ].map((item) => <li>{item.name}</li>)}</ul>;
     return (
-        <Modal>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={styles.total}>
                 <span>Total amount</span>
                 <span>35</span>
             </div>
             <div className={styles.actions}>
-                <button className={styles['button--alt']}>Close</button>
+                <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
                 <button className={styles.button}>Order</button>
             </div>
         </Modal>
